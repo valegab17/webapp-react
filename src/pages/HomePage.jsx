e3a5db552b1989c701a4297dd934b0c57563d4d9
Valeria Gabrielli <gabvale53@gmail.com>
2026-02-25 20:53:31 +0100

    feat: aggiunte chiamate verso BE

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,80 +1,38 @@
+//mi importo axios
+import axios from "axios"
+//mi importo useState e useEffect
+import { useState, useEffect, use } from "react"
+
 //mi importo la movie card per visualizzare la lista di film
 import MovieCard from "../components/MovieCard"
 
-//dati temporanei per test props
-const movies = [
-    {
-        "id": 1,
-        "title": "Inception",
-        "director": "Christopher Nolan",
-        "genre": "Science Fiction",
-        "release_year": 2010,
-        "abstract": "A skilled thief is given a chance at redemption if he can successfully perform inception.",
-        "image": "inception.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 2,
-        "title": "The Godfather",
-        "director": "Francis Ford Coppola",
-        "genre": "Crime",
-        "release_year": 1972,
-        "abstract": "The story of a powerful Italian-American crime family and their struggles.",
-        "image": "interstellar.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 3,
-        "title": "Titanic",
-        "director": "James Cameron",
-        "genre": "Romance",
-        "release_year": 1997,
-        "abstract": "A romantic story set against the tragic sinking of the RMS Titanic.",
-        "image": "matrix.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 4,
-        "title": "The Matrix",
-        "director": "The Wachowskis",
-        "genre": "Action",
-        "release_year": 1999,
-        "abstract": "A hacker discovers the truth about his reality and his role in the war against its controllers.",
-        "image": "the_godfather.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 5,
-        "title": "Interstellar",
-        "director": "Christopher Nolan",
-        "genre": "Science Fiction",
-        "release_year": 2014,
-        "abstract": "A team of explorers travels through a wormhole in space to save humanity.",
-        "image": "titanic.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    }
-]
-
-
-
+//endpoint di AXIOS
+const endpoint = "http://localhost:3000/api/movies";
 
 function HomePage() {
+    //imposto la mia variabile di stato 
+    const [movies, setMovies] = useState([]);
 
+    //funzione per gestire chiamata alla rotta index di BE
+    const fetchMovies = () =>{
+        axios.get(endpoint)
+        .then(res=>{setMovies(res.data);})
+        .catch(err =>{
+            console.log(err);
+        })
+    }
     //funzione rendering listato
     const renderMovies = () => {
         return movies.map(movie => {
             return (
                 <div className="col" key={movie.id}>
-                    <MovieCard movieProp={movie}/>
+                    <MovieCard movieProp={movie} />
                 </div>
             )
         })
     }
+    //richiamo la funzione di fetch
+    useEffect(fetchMovies,[]);
     return (
         <>
 
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,55 +1,40 @@
-import { Link } from "react-router-dom"
+//import axios
+import axios from "axios"
+//import state ed effect
+import { useState, useEffect } from "react"
+import { Link, useParams } from "react-router-dom"
+
 //import component lista reviews
 import MovieReview from "../components/MovieReview"
 
-//importo array di recensioni da postman per avere un mockup delle recensioni
-const movie = {
-    "id": 1,
-    "title": "Inception",
-    "director": "Christopher Nolan",
-    "genre": "Science Fiction",
-    "release_year": 2010,
-    "abstract": "A skilled thief is given a chance at redemption if he can successfully perform inception.",
-    "image": "inception.jpg",
-    "created_at": "2024-11-29T10:40:13.000Z",
-    "updated_at": "2025-05-22T10:55:27.000Z",
-    "reviews": [
-        {
-            "id": 1,
-            "movie_id": 1,
-            "name": "Alice",
-            "vote": 5,
-            "text": "A mind-bending masterpiece.",
-            "created_at": "2024-11-29T10:40:13.000Z",
-            "updated_at": "2024-11-29T10:40:13.000Z"
-        },
-        {
-            "id": 2,
-            "movie_id": 1,
-            "name": "Bob",
-            "vote": 4,
-            "text": "Great visuals and a compelling story.",
-            "created_at": "2024-11-29T10:40:13.000Z",
-            "updated_at": "2024-11-29T10:40:13.000Z"
-        },
-        {
-            "id": 3,
-            "movie_id": 1,
-            "name": "Charlie",
-            "vote": 3,
-            "text": "Confusing at times, but worth watching.",
-            "created_at": "2024-11-29T10:40:13.000Z",
-            "updated_at": "2024-11-29T10:40:13.000Z"
-        }
-    ]
-}
+//import endpoint
+const endpoint = "http://localhost:3000/api/movies/";
+
+
 
 
 function MoviePage() {
-    const renderReviews =() =>{
-        return movie.reviews.map(review =>{
+    //prendo il film da url rotta
+    const {id} = useParams();
+
+//setto la variabile di stato per il film
+const [movie, setMovie] = useState({});
+    //funzione per gestire chiamata alla rotta show di BE
+    const fetchMovie = () =>{
+        axios.get(endpoint + id)
+        .then(res=>{setMovie(res.data);})
+        .catch(err =>{
+            console.log(err);
+        })
+    }
+    //richiamo fetch
+        useEffect(fetchMovie,[]);
+
+    //funzione di rendering listato dei movies
+    const renderReviews = () => {
+        return movie.reviews?.map(review => {
             return (
-                <MovieReview reviewProp={review} key={review.id}/>
+                <MovieReview reviewProp={review} key={review.id} />
             )
         })
     }
